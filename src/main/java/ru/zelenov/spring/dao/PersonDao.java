87dca878c69ddc0b91c10624cb377fe820862e2d
Ivan Zelenov <Ivan_Zelenov@epam.com>
2022-05-11 21:08:29 +0300

    implement create person functionality

--- src/main/java/ru/zelenov/spring/dao/PersonDao.java
+++ src/main/java/ru/zelenov/spring/dao/PersonDao.java
@@ -8,15 +8,16 @@ import java.util.List;
 
 @Component
 public class PersonDao {
-  private List<Person> people;
-  private static int PEOPLE_COUNT;
+  private final List<Person> people;
+  private static int peopleCount;
+  private final Object lock = new Object();
 
   {
     people = new ArrayList<>();
-    people.add(new Person(++PEOPLE_COUNT, "John"));
-    people.add(new Person(++PEOPLE_COUNT, "Dayneris"));
-    people.add(new Person(++PEOPLE_COUNT, "Sansa"));
-    people.add(new Person(++PEOPLE_COUNT, "Robert"));
+    people.add(new Person(++peopleCount, "John"));
+    people.add(new Person(++peopleCount, "Dayneris"));
+    people.add(new Person(++peopleCount, "Sansa"));
+    people.add(new Person(++peopleCount, "Robert"));
   }
 
   public List<Person> index() {
@@ -28,5 +29,12 @@ public class PersonDao {
             .filter(p -> p.getId() == id)
             .findFirst()
             .orElse(null);
+  }
+
+  public void save(Person person) {
+    synchronized (lock) {
+      person.setId(++peopleCount);
+    }
+    people.add(person);
   }
 }
